2d78ce0da810ead1e399f2f2ab44da58818222fc
adamastorfranca <adamastorfranca@hotmail.com>
2022-04-05 22:20:29 -0300

    Criada página de início e cadastro de clientes

--- view/src/app/app-routing.module.ts
+++ view/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './pages/usuarios/cadastro/cadastro.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'inicio', component: InicioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { CadastroComponent } from './pages/usuarios/cadastro/cadastro.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CadastroComponent
+    CadastroComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
